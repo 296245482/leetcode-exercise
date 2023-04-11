853a09db8481267ae34406b9304c2e0582e29306
Jackie <jackie_1994@foxmail.com>
2023-04-12 00:35:34 +0800

    main init

---
+++ src/main/java/com/jackie/main.java
@@ -0,0 +1,10 @@
+package com.jackie;
+
+import com.jackie.easy.IsPalindrome;
+
+public class main {
+    public static void main(String[] args) {
+        boolean isPlain = new IsPalindrome().isPalindrome(123321);
+        System.out.println(isPlain);
+    }
+}
